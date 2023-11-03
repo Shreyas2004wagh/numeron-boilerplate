923df433643ac6541c3ff3cb6b6291119c80cdb0
Shreyas2004wagh <shreyaswagh2004@gmail.com>
2023-11-03 09:00:08 +0530

    done

--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 5: Store the player score and display it on the game over screen
+
+const scoreBox=document.getElementById("score-board");
+let passcode=localStorage.getItem("squad56");
+
+scoreBox.innerHTML=passcode;
+
+const playAgainButton=document.getElementById("play-again-button");
+playAgainButton.onclick=()=>{
+    location.href="./game.html"
+}
